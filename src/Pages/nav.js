7d0c5de1ad960e0c939a9a2a2d18dacaa513fa14
DRAHUL <86136418+Rahulgithub07@users.noreply.github.com>
2021-09-23 15:11:42 +0530

    duplicates

--- src/Pages/nav.js
+++ src/Pages/nav.js
@@ -9,9 +9,9 @@ return(
     <Navbar.Brand href="#home">PostGram</Navbar.Brand>
     <Nav className="ms-auto ">
 
-      <Nav.Link ><NavLink className="link" activeClassName="selected" exact to="/" activeClassName="selected">Home</NavLink></Nav.Link>
-      <Nav.Link ><NavLink  className="link" activeClassName="selected" to="/posts" activeClassName="selected">Posts</NavLink></Nav.Link>
-      <Nav.Link ><NavLink  className="link" activeClassName="selected" to="/about" activeClassName="selected">About</NavLink></Nav.Link>
+      <Nav.Link ><NavLink className="link" activeClassName="selected" exact to="/" >Home</NavLink></Nav.Link>
+      <Nav.Link ><NavLink  className="link" activeClassName="selected" to="/posts" >Posts</NavLink></Nav.Link>
+      <Nav.Link ><NavLink  className="link" activeClassName="selected" to="/about" >About</NavLink></Nav.Link>
     </Nav>
     </Container>
   </Navbar>
